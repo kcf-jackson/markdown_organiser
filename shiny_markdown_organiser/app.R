51038cfa9854dbb1bb31a356718a3baa9fd09ffb
kcf-jackson <kcf.jackson@gmail.com>
2021-05-05 11:26:50 +1000

    Shiny app

---
+++ shiny_markdown_organiser/app.R
@@ -0,0 +1,65 @@
+# remotes::install_github("kcf-jackson/sketch", "experiment")
+library(sketch)
+library(shiny)
+source("calendar.R")
+
+
+# Compile R files on-the-fly
+setwd("./src")
+file.copy(
+    source_r("main.R", launch_browser = NULL),
+    "../www/index.html", overwrite = T
+)
+setwd("../")
+
+
+# Define server logic to handle templates and file IO
+server <- function(input, output, session) {
+    observeEvent(input$request,{
+         req <- input$request
+         if (req$type == "template_page") {
+             if (req$message == "kanban") {
+                 template <- file_to_json("./data/kanban.md")
+             }
+             if (req$message == "daily-planner") {
+                 template <- file_to_json("./data/daily_planner.md")
+             }
+             if (req$message == "weekly-planner") {
+                 template <- file_to_json("./data/weekly_planner.md")
+             }
+             if (req$message == "monthly-planner") {
+                 template <- jsonlite::toJSON(
+                     paste(
+                         paste(readLines("./data/monthly_planner.md"),
+                               collapse = "\n"),
+                         calendar_table(this_year(), this_month()),
+                         sep = "\n"
+                     ),
+                     auto_unbox = TRUE
+                 )
+             }
+             session$sendCustomMessage("render_template", template)
+         }
+
+         if (req$type == "help_page") {
+             help_desc <- file_to_json("./data/description.md")
+             session$sendCustomMessage("render_help", help_desc)
+         }
+
+         if (req$type == "setup") {
+             textarea_desc <- file_to_json("./data/sample_input.md")
+             session$sendCustomMessage("render_textarea", textarea_desc)
+         }
+    })
+}
+
+file_to_json <- function(x) {
+    jsonlite::toJSON(
+        paste(readLines(x), collapse = "\n"),
+        auto_unbox = TRUE
+    )
+}
+
+
+# Run the application
+shinyApp(ui = htmlTemplate("./www/index.html"), server)
---
+++ shiny_markdown_organiser/calendar.R
@@ -0,0 +1,30 @@
+calendar_dates <- function(year, month) {
+    num_days <- function(m) {
+        c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)[m]
+    }
+
+    s <- seq(num_days(month))
+    new_date <- function(day) {
+        date <- as.Date(glue::glue("{year}-{month}-{day}"))
+        format(date, "%b %d (%a)")
+    }
+    unlist(Map(new_date, s))
+}
+
+calendar_table <- function(year, month) {
+    tr <- function(date) glue::glue("| {date} | \t |")
+    res <- c("### ", "| Date | Plan |", "| --- | --- |",
+             unlist(Map(tr, calendar_dates(year, month))))
+    paste(res, collapse = "\n")
+}
+
+this_year <- function() {
+    as.numeric(format(Sys.Date(), "%Y"))
+}
+
+this_month <- function() {
+    as.numeric(format(Sys.Date(), "%m"))
+}
+
+# Example
+# cat(calendar_table(2021, 5))
